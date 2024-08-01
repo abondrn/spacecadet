15387187042342ee1394be79da43fcc69f7ffb54
Alex <abondarenko101@gmail.com>
2024-08-01 04:14:46 -0700

    Added tts and graceful exit

--- src/quiz.cljs
+++ src/quiz.cljs
@@ -1,16 +1,17 @@
-(ns src.main
+(ns src.quiz
   (:require
    [clojure.pprint :as pprint]
    [clojure.string :as str]
-   
+
    [promesa.core :as p]
    [applied-science.js-interop :as j]
    [nbb.repl :as repl]
-   
+
+   ["child_process$default" :as proc]
    ["inquirer$default" :as inquirer]
    ["chalk$default" :as chalk]
    ["fast-diff$default" :as diff]
-   
+
    [src.kana :as k]
    [src.num :as num]))
 
@@ -21,8 +22,8 @@
   (let [am-or-pm (rand-int 2)
         h (num/randrange 1 12 1)
         m (num/randrange 0 55 5)
-        jh (num/jnumber "hour" h)
-        jm (num/jnumber "minute" m)
+        jh (num/jnumber "hours" h)
+        jm (num/jnumber "minutes" m)
         hir (str (am-pm am-or-pm) " " jh " " jm)
         rom (k/romanize hir)]
     {:am-or-pm am-or-pm
@@ -30,19 +31,24 @@
      :m m
      :eng (pprint/cl-format nil "~d:~2,'0d ~d" h m (get ["a.m." "p.m."] am-or-pm))
      :hir hir
-     :rom rom
-     }))
+     :rom rom}))
+
+(def exit (atom false))
+
+(defn handle-sigint []
+  (println "Exiting...")
+  (reset! exit true))
 
 (defn prompt [questions]
-  (inquirer/prompt (clj->js questions)))
+  (p/catch (inquirer/prompt (clj->js questions))
+           (fn [error]
+             (handle-sigint))))
 
 (defn strip-whitespace [s]
   (str/replace s #"\s+" ""))
 
 ; https://teamjapanese.com/stop-in-japanese/
 (defonce Q ["quit" "stop" "exit" "q" "yamete" "yamero" "tomare" "mouii" "sutoppu"])
-
-(def exit (atom false))
 
 (defn in? [xs el] (some #(= % el) xs))
 
@@ -56,32 +62,32 @@
   (if (string? answer-or-answers)
     (diff input answer-or-answers)
     (apply min-key
-     (fn [d] (sum (for [change d :when (ne-change change)] (count (get change 1)))))
-     (map #(diff input %) answer-or-answers))))
+           (fn [d] (sum (for [change d :when (ne-change change)] (count (get change 1)))))
+           (map #(diff input %) answer-or-answers))))
+
+(defn tts [text]
+  (proc/execSync (str "say -v Kyoko '" text "'")))
 
 (defn diff-attempt [input answer message]
   ;(println "\n" input answer)
   (let [raw-diff (diffs input answer)
         stripped-diff (filter ne-change raw-diff)]
     (cond ; no diff
-          (== 0 (count stripped-diff)) true
+      (== 0 (count stripped-diff)) true
           ; show answer
-          (in? ["idk" "?"] input) (do (println "\n" answer) true)
+      (in? ["idk" "?"] input) (do (println "\n" answer) true)
           ; should quit
-          (in? Q input) (do (reset! exit true) true)
-          (in? ["rom"] input) (k/romanize message)
-          :else 
-          (apply str (for [change raw-diff
-                            :let [code (get change 0)
-                                  tokens (get change 1)]]
-                        (case code
-                          0  (chalk/green tokens)
-                          1  (->> "?" (repeat (count (strip-whitespace tokens))) (apply str) chalk/italic chalk/yellow)
-                          -1 (-> tokens chalk/red chalk/strikethrough)))))))
-
-(defn handle-sigint []
-  (println "Exiting...")
-  (reset! exit true))
+      (in? Q input) (do (reset! exit true) true)
+      (in? ["rom"] input) (k/romanize message)
+      (in? ["say" "speak" "aloud" "tts"] input) (do (tts message) "")
+      :else
+      (apply str (for [change raw-diff
+                       :let [code (get change 0)
+                             tokens (get change 1)]]
+                   (case code
+                     0  (chalk/green tokens)
+                     1  (->> "?" (repeat (count (strip-whitespace tokens))) (apply str) chalk/italic chalk/yellow)
+                     -1 (-> tokens chalk/red chalk/strikethrough)))))))
 
 (defn quiz-loop [target message]
   (p/let [start (.now js/Date)
@@ -122,48 +128,67 @@
   (let [jtime (rand-time)]
     (quiz-loop (:rom jtime) (str "Translate to Japanese: " (:eng jtime)))))
 
-(defonce OBJECTS
-  [{:eng "business card" :hir "めいし"}
-   {:eng "umbrella" :hir "かさ"}
-   {:eng "book" :hir "ほん"}
-   {:eng "file" :kat "ファイル"}
-   {:eng "glasses" :hir "めがね"}
-   {:eng "smart phone" :kat "スマホ"}
-   {:eng "key" :hir "かぎ"}
-   {:eng "wallet" :hir "さいふ"}
-   {:eng "pen" :kat "ペン"}
-   {:eng "telephone" :hir "でんわ"}
-   {:eng "bag" :hir " かばん"}
-   {:eng ["watch" "clock"] :hir "とけい"}
-   
-   {:eng "television" :kat "テレビ"}
-   {:eng "computer" :kat "パソコン"}
-   {:eng "refrigerator" :hir "れいぞうこ"}
-   {:eng "air conditioner" :kat "エアコン"}
-   {:eng "microwave oven" :hir "でんし れんじ"}
+(defn quiz-terms [terms]
+  (fn []
+    (let [questions (for [obj terms
+                          :let [eng (:eng obj)
+                                jap (or (:kat obj) (:hir obj))]
+                          e2j? [true false]]
+                      (if e2j?
+                        [(str "Translate to Japanese: " (if (string? eng) eng (str/join "/" eng))) (k/romanize jap)]
+                        [(str "Translate to English: " jap) eng]))
+          log (transient [])]
+      (p/loop [curr-questions (shuffle questions) next-questions []]
+        (cond (not-empty curr-questions) (p/let [[msg target] (first curr-questions)
+                                                 res (quiz-loop target msg)]
+                                           (conj! log res)
+                                           (if (zero? (:reattempts res))
+                                             (p/recur (rest curr-questions) next-questions)
+                                             (p/recur (rest curr-questions) (conj next-questions [msg target]))))
+              (not-empty next-questions) (p/recur (shuffle next-questions) [])
+              :else (persistent! log))))))
 
-   {:eng "coffee" :kat "コーヒー"}
-   {:eng "juice" :kat "ジュース"}
-   {:eng "sandwich" :kat "サンドイチ"}
-   {:eng "black tea" :hir "こうちゃ"}
-   ])
+(defonce quiz-objects
+  (quiz-terms
+   [{:eng "business card" :hir "めいし"}
+    {:eng "umbrella" :hir "かさ"}
+    {:eng "book" :hir "ほん"}
+    {:eng "file" :kat "ファイル"}
+    {:eng "glasses" :hir "めがね"}
+    {:eng "smart phone" :kat "スマホ"}
+    {:eng "key" :hir "かぎ"}
+    {:eng "wallet" :hir "さいふ"}
+    {:eng "pen" :kat "ペン"}
+    {:eng "telephone" :hir "でんわ"}
+    {:eng "bag" :hir " かばん"}
+    {:eng ["watch" "clock"] :hir "とけい"}
+    {:eng "card" :kat "カード"}
+    {:eng "ballpoint pen" :kat "ボールペン"}
 
-(defn quiz-objects []
-  (let [questions (for [obj OBJECTS
-                        :let [eng (:eng obj)
-                              jap (or (:kat obj) (:hir obj))]
-                        e2j? [true false]]
-                    (if e2j?
-                      [(str "Translate to Japanese: " (if (string? eng) eng (str/join "/" eng))) (k/romanize jap)]
-                      [(str "Translate to English: " jap) eng]))]
-    (p/loop [curr-questions (shuffle questions) next-questions []]
-      (cond (not-empty curr-questions) (p/let [[msg target] (first curr-questions)
-                                                res (quiz-loop target msg)]
-                                         (if (zero? (:reattempts res))
-                                           (p/recur (rest curr-questions) next-questions)
-                                           (p/recur (rest curr-questions) (conj next-questions [msg target]))))
-            (not-empty next-questions) (p/recur (shuffle next-questions) [])
-            :else 'done))))
+    {:eng "television" :kat "テレビ"}
+    {:eng "computer" :kat "パソコン"}
+    {:eng "refrigerator" :hir "れいぞうこ"}
+    {:eng "air conditioner" :kat "エアコン"}
+    {:eng "microwave oven" :hir "でんし れんじ"}
+    {:eng "toaster" :kat "トースター"}
+
+    {:eng "coffee" :kat "コーヒー"}
+    {:eng "juice" :kat "ジュース"}
+    {:eng "sandwich" :kat "サンドイチ"}
+    {:eng "black tea" :hir "こうちゃ"}
+    {:eng "salad" :kat "サラダ"}]))
+
+(defonce quiz-events
+  (quiz-terms
+   [{}]))
+
+(defonce quiz-locations
+  (quiz-terms
+   []))
+
+(defonce quiz-expressions
+  (quiz-terms
+   []))
 
 (defn -main []
   (println "Press Ctrl+C to exit.")
@@ -179,7 +204,10 @@
                  "time" quiz-time
                  "phone numbers" quiz-phone
                  "numbers 1-100000" quiz-numbers
-                 "objects" quiz-objects)]
+                 "objects" quiz-objects
+                 "events" quiz-events
+                 "locations" quiz-locations
+                 "expressions" quiz-expressions)]
     (p/loop []
       (quiz)
       (when (not @exit)
